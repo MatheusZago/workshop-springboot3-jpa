4047c0fbdd3e65d6976bb5c341ab69672166e4fe
Matheus Lui <matheuszago134@gmail.com>
2024-09-04 15:16:38 -0300

    User insert

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
@@ -33,5 +37,11 @@ public class UserResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
-	
+	@PostMapping //é para inserir com o method post http
+	public ResponseEntity<User> insert(@RequestBody User obj){ //Precisa desse body para o JSon virar obj Java
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri(); //Pra pegar o objeto valor URI
+		return ResponseEntity.created(uri).body(obj); //Vai retornar o User e o código de criação 201
+	}
 }
